920b21ec1cb51c86830e5e3bb9acdd5dc7a78332
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2020-11-17 20:28:57 -0600

    Update script to use MSGraph commands

--- directory-services/azure-active-directory/create-azure-app-registration/scripts/azuread_sample.ps1
+++ directory-services/azure-active-directory/create-azure-app-registration/scripts/azuread_sample.ps1
@@ -1,50 +1,68 @@
-<# Connect to your tenant #>
+<#
+    Connect to your tenant
+#>
 $tenantId = ''
 Connect-AzureAd -TenantId $tenantId
 
-<# Variables - Adjust for your environment/requirements #>
-$appName = "Thycotic Secret Server"
+<#
+    Variables - Adjust for your environment/requirements
+#>
+$appName = "Thycotic Secret Server2"
 $appRedirect = "https://vault.company.com/signin-oidc"
-$yearsValidSecret = 3
 
-# do not change
+<#
+    DO NOT CHANGE
+#>
 $appPerms = 'Group.Read.All','GroupMember.Read.All','Member.Read.Hidden','User.Read.All'
 
-<# Create the App Registration & Assigning Microsoft Graph Permissions #>
-# Pull the Service App ID for Microsoft Graph
+<#
+    Pull the Service App ID for Microsoft Graph
+#>
 $msGraphService = Get-AzureADServicePrincipal -Filter "DisplayName eq 'Microsoft Graph'"
 
-# Create object for Resource Access - assigning app role permissions
-$msGraphResourceAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+<#
+    Create object for Resource Access - assigning app role permissions
+#>
+$msGraphResourceAccess = New-Object -TypeName "Microsoft.Open.MSGraph.Model.RequiredResourceAccess"
 $msGraphResourceAccess.ResourceAppId = $msGraphService.AppId
-# This grabs the ID for each permission listed in $appPerms variable
+
+<#
+    This grabs the ID for each permission listed in $appPerms variable
+#>
 $permissions = $msGraphService.AppRoles.Where({$_.Value -in $appPerms})
 foreach ($p in $permissions) {
-    $appPermissions = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $p.Id,"Role"
-    #Add the role to the resource access object
+    $appPermissions = New-Object -TypeName "Microsoft.Open.MSGraph.Model.ResourceAccess" -ArgumentList $p.Id,"Role"
+    <# Add the role to the resource access object #>
     $msGraphResourceAccess.ResourceAccess += $appPermissions
 }
 
-# Create the App Registration
+<#
+    Create the App Registration
+#>
 $paramsApp = @{
     DisplayName = $appName
-    ReplyUrls = $appRedirect
+    Web = @{ RedirectUris = $appRedirect }
     RequiredResourceAccess = $msGraphResourceAccess
-    Oauth2AllowImplicitFlow = $true
 }
-$thycoticApp = New-AzureADApplication @paramsApp
+$thycoticApp = New-AzureADMSApplication @paramsApp
 
-<# Create the Client Secret and assign to the App Registration created #>
+<#
+    Create the Client Secret and assign to the App Registration created
+
+    !!NOTE!! MSGraph only supports the expiration being set to 2 years, no configuration option is provided
+#>
 $paramsClientSecret = @{
-    ObjectId = $thycoticApp.ObjectId
-    EndDate = (Get-Date).AddYears($yearsValidSecret)
-    CustomKeyIdentifier = "Secret Server $(Get-Date -Format yyyy-MM-dd)"
+    ObjectId = $thycoticApp.Id
+    PasswordCredential = @{ displayName = "Secret Server $(Get-Date -Format yyyy-MM-dd)"}
 }
-$clientSecret = New-AzureADApplicationPasswordCredential @paramsClientSecret
+$clientSecret = New-AzureADMSApplicationPassword @paramsClientSecret
 
+<#
+    Output object data needed for configuring Secret Server
+#>
 [pscustomobject]@{
     Details = "These values required for Secret Server Configuration"
     TenantId = (Get-AzureADTenantDetail).ObjectId
     ClientID = $thycoticApp.AppId
-    ClientSecret = $clientSecret.Value
+    ClientSecret = $clientSecret.SecretText
 } | Format-List
